cae4341dc9164db796bf226f1b842a0648d2e68b
tanak <tanakajapanese39@gmail.com>
2026-06-01 15:25:11 +0900

    Initial commit

---
+++ src/main/kotlin/com/eastcompany/eastsub/jinro/Jinro.kt
@@ -0,0 +1,14 @@
+package com.eastcompany.eastsub.jinro
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Jinro : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
